956d7ff22ec60f7f8089275aacc7e4111d56ee44
v-kazantsev <vvs201@mail.ru>
2018-03-07 18:08:25 +1000

    добавил хелпер truncate

--- lib/blog_web/views/blog_view.ex
+++ lib/blog_web/views/blog_view.ex
@@ -1,5 +1,5 @@
 defmodule BlogWeb.BlogView do
   use BlogWeb, :view
-  import Blog.Publications.Helpers, only: [time_ago_in_words: 1]
+  import Blog.Publications.Helpers, only: [time_ago_in_words: 1, truncate: 2]
   import Scrivener.HTML
 end
